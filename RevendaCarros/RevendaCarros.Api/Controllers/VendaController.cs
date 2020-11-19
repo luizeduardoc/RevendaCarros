9d6620b20267e63ecedbc71f9e0f55afc78ae05e
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2020-11-19 19:49:12 -0300

    Change folder name to RevandaCarros.Api

---
+++ RevendaCarros/RevendaCarros.Api/Controllers/VendaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RevendaCarros.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class VendaController : Controller
+    {        
+        public VendaController()
+        {            
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = "Ok";
+            return Ok(result);
+        }
+    }
+}
