7002ae5c51292926f9fdd6d2435ea343d19fa92b
Renata300 <renatamottin357@gmail.com>
2020-11-20 21:51:35 -0200

    Adicionando venda e aluguel

---
+++ RevendaCarros/RevendaCarros.Domain/Entities/Vendas.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RevendaCarros.Domain.Entities
+{
+    [Table("vendass")]
+    public class Vendas : IEntity
+    {
+        public int IdCarro { get; set; }
+        public double Valor { get; set; }
+        public string NomeComprador { get; set; }
+    }
+}
---
+++ RevendaCarros/RevendaCarros.Infrastructure/Repositories/AluguelRepository.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManager.Infrastructure.Core;
+using RevendaCarros.Domain.Entities;
+using RevendaCarros.Domain.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RevendaCarros.Infrastructure.Repositories
+{
+    public class AluguelRepository : Repository<Alugueis>, IAluguelaRepository
+    {
+        public AluguelRepository(RevendaCarrosContext context) : base(context)
+        {
+        }
+        public IList<Alugueis> GetAll()
+        {
+            return Query().ToList();
+        }
+    }
+}
