319cecfb7238c00cceaeca7ee7a259c8d51719ea
Renata300 <renatamottin357@gmail.com>
2020-11-26 19:59:32 -0200

    ajustando aluguel

--- RevendaCarros/RevendaCarros.Api/Controllers/AluguelController.cs
+++ RevendaCarros/RevendaCarros.Api/Controllers/AluguelController.cs
@@ -1,19 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RevendaCarros.Domain.Dtos;
+using RevendaCarros.Domain.Services.Interfaces;
 
 namespace RevendaCarros.Api.Controllers
 {
     [Route("api/[controller]")]
     public class AluguelController : Controller
     {
-        public AluguelController()
-        {
+        private readonly IAluguelService service;
 
+        public AluguelController(IAluguelService service)
+        {
+            this.service = service;
         }
 
         [HttpGet]
         public IActionResult GetAll()
         {
             var result = "ok";
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] AluguelDto aluguelDto)
+        {
+            var result = service.Create(aluguelDto);
             return Ok(result);
         }
     }
